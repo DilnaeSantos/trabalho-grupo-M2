6656c288f75add4177954e7bfd5704febd60b281
IgorSPinto <igorpinto103@gmail.com>
2023-07-07 17:34:58 -0300

    feat: :sparkles: validação dos campos de login
    introduzido validação em JS dos campos de usuário e senha de login

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,4 +1,41 @@
 document.getElementById('submit').addEventListener('click', function(){
     var usuario = document.getElementById('usuario').value;
     var senha = document.getElementById('senha').value;
-})
+
+    if(!validarEmail(usuario)) {
+        alert('Email inválido!');
+        return;
+    }
+
+    if(!validarSenha(senha)) {
+        alert('Senha inválida! A senha deve ter de 8 a 13 caracteres e pelo menos um caracter especial!');
+        return;
+    }
+
+
+    alert('Formulário Válido!');
+});
+
+function validarEmail(email) {
+    var atPos = email.indexOf('@');
+    if (atPos < 4 || email.length - 1 < 8) {
+        return false;
+    }
+
+    return true;
+}
+
+function validarSenha(){
+    if (senha.length < 8 || senha.length > 13) {
+        return false;
+    }
+
+    var caracterEspecial = '!@#$%^&*';
+    for (var i = 0; i < senha.length; i++){
+        if (caracterEspecial.indexOf(senha.charAt(i)) > -1) {
+            return true;
+        }
+    }
+
+    return false;
+}
