d4a7f74afef532c0b159287abc02db17cc7ac77f
IgorSPinto <igorpinto103@gmail.com>
2023-07-07 20:01:43 -0300

    fix: :bug: corrigido bug da validação
    corrigido o valida senha

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,41 +1,37 @@
-document.getElementById('submit').addEventListener('click', function(){
+document.getElementById('submit').addEventListener('click', function () {
     var usuario = document.getElementById('usuario').value;
     var senha = document.getElementById('senha').value;
 
-    if(!validarEmail(usuario)) {
+    if (!validarEmail(usuario)) {
         alert('Email inválido!');
         return;
     }
 
-    if(!validarSenha(senha)) {
-        alert('Senha inválida! A senha deve ter de 8 a 13 caracteres e pelo menos um caracter especial!');
+    if (!validarSenha(senha)) {
+        alert('Senha inválida! A senha deve ter de 8 a 13 caracteres e pelo menos um caractere especial.');
         return;
     }
 
-
-    alert('Formulário Válido!');
+    alert('Formulário válido!'); // Apenas para fins de exemplo
 });
 
 function validarEmail(email) {
     var atPos = email.indexOf('@');
-    if (atPos < 4 || email.length - 1 < 8) {
+    if (atPos < 4 || email.length - atPos - 1 < 8) {
         return false;
     }
-
     return true;
 }
 
-function validarSenha(){
+function validarSenha(senha) {
     if (senha.length < 8 || senha.length > 13) {
         return false;
     }
-
-    var caracterEspecial = '!@#$%^&*';
-    for (var i = 0; i < senha.length; i++){
-        if (caracterEspecial.indexOf(senha.charAt(i)) > -1) {
+    var specialChars = '!@#$%^&*';
+    for (var i = 0; i < senha.length; i++) {
+        if (specialChars.indexOf(senha.charAt(i)) > -1) {
             return true;
         }
     }
-
     return false;
 }
