ce8ea94b4b2f15f95af5da488bc5b7ad41fd1725
IgorSPinto <igorpinto103@gmail.com>
2023-07-11 18:36:22 -0300

    feat: :sparkles: mudança para POO
    criado a classe LoginForm que irá receber as informações dos dados de login

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,39 +1,6 @@
-document.getElementById('submit').addEventListener('click', function () {
-    var usuario = document.getElementById('usuario').value;
-    var senha = document.getElementById('senha').value;
-
-    if (!validarEmail(usuario)) {
-        alert('Email inválido!');
-        return;
+class LoginForm {
+    constructor(){
+        this.usuario = '';
+        this.senha = '';
     }
-
-    if (!validarSenha(senha)) {
-        alert('Senha inválida! A senha deve ter no mínimo 8 caracteres e pelo menos um caractere especial.');
-        return;
-    }
-
-    alert('Formulário válido!'); 
-
-    window.location.href = '../html/dashboard.html';
-});
-
-function validarEmail(email) {
-    var atPos = email.indexOf('@');
-    if (atPos < 4 || email.length - atPos - 1 < 8) {
-        return false;
-    }
-    return true;
-}
-
-function validarSenha(senha) {
-    if (senha.length < 8) {
-        return false;
-    }
-    var specialChars = '!@#$%^&*';
-    for (var i = 0; i < senha.length; i++) {
-        if (specialChars.indexOf(senha.charAt(i)) > -1) {
-            return true;
-        }
-    }
-    return false;
 }
