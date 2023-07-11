98ae868becedad591fd4668ba6f53cb4ff914f05
IgorSPinto <igorpinto103@gmail.com>
2023-07-11 18:43:22 -0300

    feat: :sparkles: método validarEmail adicionado
    adicionado o método de validação do Email

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,6 +1,16 @@
+/* classe do FOrmulário */
 class LoginForm {
     constructor(){
         this.usuario = '';
         this.senha = '';
     }
+
+    /* validação do email */
+    validarEmail() {
+        var atPos = this.usuario.indexOf('@');
+        if (atPos < 4 || this.usuario.length - atPos - 1 < 8) /* garante que o email tenha pelo menos 3 caracteres antes do @, e que após o @ tenha pelo menos 8 caracteres (gmail.com, hotmail.com) */
+            return false;
+    }
+
+    
 }
