93700cd4c89b57d59c9506549611513f87ee76df
DilnaeSantos <116980369+DilnaeSantos@users.noreply.github.com>
2023-07-12 21:19:42 -0300

    foi adicionado o js da pagina de contato

---
+++ src/models/contato.js
@@ -0,0 +1,21 @@
+const btnEnvio = document.querySelector('#enviarBtn')
+
+function comfirmação (event) {
+    event.preventDefault()
+
+    let nome = document.querySelector('#nome').value
+    let email = document.querySelector('#email').value
+    let msg = document.querySelector('#msg').value
+    let msgDeEnvio = document.querySelector('#msgDeEnvio')
+    
+  if (nome === '' || email === '' || msg === '') {
+    console.log('erro campo invalido')
+  } else {
+    msgDeEnvio.textContent = 'E-mail enviado com sucesso'
+    setTimeout(() => {
+      document.querySelector('form').submit()
+    },3000)
+  }
+}
+
+btnEnvio.addEventListener('click', comfirmação)
